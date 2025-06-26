57f0fed249a1b2c592422b48dfdc9eeba024305d
fadilsyy <fadilasytar@gmail.com>
2025-06-26 15:24:49 +0700

    step 7a

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -73,6 +73,13 @@ public:
             // 6b: Exit
             return;
         }
+        // Step 7: if the value in the data field of the new node is greater than that of parent
+        else if(x > parent->info) {
+            // 7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+
+        }
 
     }
 };
