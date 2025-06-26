88534118cc89572e7b73e9076bc45026c93bc0fd
fadilsyy <fadilasytar@gmail.com>
2025-06-26 14:59:18 +0700

    class node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* leftchild;
+    Node* rightchild;
+
+    // Constructor for the node class
+    Node(int value) {
+        data = value;
+        leftchild = nullptr;    // Initialize left child to null
+        rightchild = nullptr;   // Initialize right child to null
+    }
+};
+
