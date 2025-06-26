aea84bb690c479982fdf2dbeb331f590b42c5f89
fadilsyy <fadilasytar@gmail.com>
2025-06-26 15:20:01 +0700

    step 5a

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -58,7 +58,8 @@ public:
 
         // Step 5: If parent is NULL (Tree is empty)
         if(parent ==  nullptr) {
-
+            // 5a: Mark the new node as ROOT
+            ROOT = newNode;
         }
 
     }
